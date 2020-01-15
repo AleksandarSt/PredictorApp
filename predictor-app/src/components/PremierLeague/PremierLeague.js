79f34f59aa631205072f4955e6c12efd6dc606d2
Aleksandar Stoyanov <aleksandar.v.stoyanov@gmail.com>
2020-01-16 00:11:50 +0200

    hardcoded prediction rows

--- predictor-app/src/components/PremierLeague/PremierLeague.js
+++ predictor-app/src/components/PremierLeague/PremierLeague.js
@@ -2,6 +2,58 @@ import React from "react";
 import { Table, Input, FormGroup, Label } from "reactstrap";
 
 const PremierLeague = () => {
+  const predictions = [
+    {
+      id: 1,
+      scheduledOn: "20.01.2020 15:00",
+      homeTeamName: "Manchester United",
+      homePrediction: 3,
+      awayPrediction: 2,
+      awayTeamName: "Manchester city",
+      finalScore: "3-2",
+      userPoints: 2
+    },
+    {
+      id: 1,
+      scheduledOn: "20.01.2020 15:00",
+      homeTeamName: "Manchester United",
+      homePrediction: 3,
+      awayPrediction: 2,
+      awayTeamName: "Manchester city",
+      finalScore: "3-2",
+      userPoints: 2
+    },
+    {
+      id: 1,
+      scheduledOn: "20.01.2020 15:00",
+      homeTeamName: "Manchester United",
+      homePrediction: 3,
+      awayPrediction: 2,
+      awayTeamName: "Manchester city",
+      finalScore: "3-2",
+      userPoints: 2
+    }
+  ];
+
+  const predictionRows = predictions.map(p => {
+    return (
+      <tr>
+        <td>{p.scheduledOn}</td>
+        <td>{p.homeTeamName}</td>
+        <td>
+          <form class="form-inline">
+            <Input className="form-control w-25" />
+            <span class="input-group-text">-</span>
+            <Input className="form-control w-25" />
+          </form>
+        </td>
+        <td>{p.awayTeamName}</td>
+        <td>{p.finalScore}</td>
+        <td>{p.userPoints}</td>
+      </tr>
+    );
+  });
+
   return (
     <>
       <FormGroup>
@@ -17,25 +69,15 @@ const PremierLeague = () => {
       <Table>
         <thead>
           <tr>
+            <th>Data i chas</th>
             <th>Otbor</th>
-            <th>prognoza1</th>
-            <th>Prognoza2</th>
+            <th>Prognoza</th>
             <th>otbor2</th>
+            <th>rezultat</th>
+            <th>tochki</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>Manchester United</td>
-            <td>
-              <form class="form-inline">
-                <Input className="form-control w-25" />
-                <span class="input-group-text">-</span>
-                <Input className="form-control w-25" />
-              </form>
-            </td>
-            <td>Noric</td>
-          </tr>
-        </tbody>
+        <tbody>{predictionRows}</tbody>
       </Table>
     </>
   );
